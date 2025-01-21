ef5a49a3bf085a50b737baf61b463882b2fdd185
Deepu22-Dev <deepthiburri2211@gmail.com>
2025-01-21 16:39:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import LanguageSelector from "./components/language-selector";
+import {Trans, useTranslation} from "react-i18next";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const {t} = useTranslation();
+  const {line1, line2} = t("description", {channel: "RoadsideCoder"});
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="container">
+      <LanguageSelector />
+      <h1>{t("greeting")}</h1>
+      <span>
+        <Trans
+          // i18nKey={"description.line1"}
+          i18nKey={line1}
+          values={{
+            channel: "RoadsideCoder",
+          }}
+          components={{1: <b />}}
+        ></Trans>
+      </span>
+      <span>{line2}</span>
+      {/* <span>{t("greeting.key", "can't load")}</span> */}
+    </div>
+  );
+};
 
-export default App
+export default App;
