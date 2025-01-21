7efac510124f051975a1089e46cd6ca0e5ebb990
Deepu22-Dev <deepthiburri2211@gmail.com>
2025-01-21 16:34:53 +0530

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
+import "./i18n/i18n.js";
 
-
-
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <React.Suspense fallback="loading...">
+      <App />
+    </React.Suspense>
+  </React.StrictMode>
+);
